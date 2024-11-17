41724d2736cd3f57ad078277122b85a4c5c8f6cd
topmbr <centerm9911@gmail.com>
2024-11-17 21:27:27 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Повернута ли строка?
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(IsStringRotated("javascript", "scriptjava")); // true
+        Console.WriteLine(IsStringRotated("javascript", "iptjavascr")); // true
+        Console.WriteLine(IsStringRotated("javascript", "java"));       // false
+    }
+
+    static bool IsStringRotated(string source, string test)
+    {
+        if (source.Length != test.Length)
+        {
+            return false;
+        }
+
+        string doubledSource = source + source;
+
+        return doubledSource.Contains(test);
+    }
+}
---
+++ ConsoleApp115/Program.cs
@@ -0,0 +1,30 @@
+﻿//Плосткий массив
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        var nestedArray = new object[] { new object[] { 1 }, new object[] { 2, 3 }, new object[] { new object[] { 4 } } };
+        var flattenedArray = Flatten(nestedArray);
+
+        Console.WriteLine(string.Join(", ", flattenedArray)); 
+    }
+
+    static List<int> Flatten(object[] array)
+    {
+        var result = new List<int>();
+        foreach (var element in array)
+        {
+            if (element is object[] nestedArray)
+            {
+                result.AddRange(Flatten(nestedArray)); 
+            }
+            else if (element is int number)
+            {
+                result.Add(number); 
+            }
+        }
+        return result;
+    }
+}
